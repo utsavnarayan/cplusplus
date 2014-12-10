60e202701c9f18cab18618f8c7d6ae66904355f7
Utsav Narayan Singh <vastusingh@gmail.com>
2014-12-09 20:15:55 -0800

    Added a program to generate random numbers

---
+++ random.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cstdlib>
+ 
+using namespace std;
+ 
+main()
+{
+   int n, max, num, c;
+ 
+   cout << "Enter the number of random numbers you want ";
+   cin >> n;
+   cout << "Enter the maximum value of random number ";
+   cin >> max;   
+ 
+   cout << "random numbers from 0 to " << max << " are :-" << endl;
+ 
+   for ( c = 1 ; c <= n ; c++ )
+   {
+      num = random(max);
+      cout << num << endl;
+   }         
+ 
+   return 0;
+}
